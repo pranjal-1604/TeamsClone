544a0010509da5d4a2a83e480cbaec300f4e35d2
Pranjal Singh <pranjal.dalu@gmail.com>
2021-06-30 11:57:46 +0530

    changes

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://ms-teams-clone-video-chat.herokuapp.com/');
 // const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
